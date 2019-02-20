c393e46a0f70a95bbe839e0844c253051f7944b7
VictorPaco <42852043+VictorPaco@users.noreply.github.com>
2019-02-20 11:26:03 -0300

    Alteração em linhas de codigos
    Remoção de echos em duplicidades e implementação de quebra de linha

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php 
 
-	echo "Ola Mundo! Abraço 99";
-	echo "Nois  Filhão 99";
-	echo "Testando o Git 99";
-	echo "Testando o Git2 99";
-	echo "ola Paco 99 e ola Paco 22";
+	echo "Ola Mundo! Abraço";
+	echo "<br>";
+	echo "Nois  Filhão";
+	echo "<br>";
+	echo "Testando o Git";
+	echo "<br>";
+	echo "ola Paco e ola Pac ";
+	echo "<br>";
 	echo "Testando mais alterações";
-	echo "Testando mais alterações2";
-	echo "Testando mais alterações3";
  ?>
