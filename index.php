2d689dfd2178ca0f0371220fc9839dcf5e023406
Victor Paco <v.pacoinformatica@gmail.com>
2019-02-20 11:01:20 -0300

    tentando desfazer a merda

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
 	echo "Nois  Filhão 99";
 	echo "Testando o Git 99";
 	echo "Testando o Git2 99";
-	echo "ola Paco 99 e ola Paco 22"
-
+	echo "ola Paco 99 e ola Paco 22";
+	echo "Testando mais alterações";
  ?>
